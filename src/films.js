ff4ae8817a0666c06837e2ea77493ac67e8ab53d
Newbiecodeweaver <carlosgilino@gmail.com>
2023-04-18 12:52:15 +0200

    implement getMoviesFromDirector function to show movies from a specific director

--- src/films.js
+++ src/films.js
@@ -1,6 +1,5 @@
 const mainContent = document.querySelector("main")
 
-
 function getAllDirectors() {
 
   const arrayDirectors = movies.map(movie => movie.director);
@@ -22,9 +21,26 @@ function showDirectors() {
 
   arrayDirectors.forEach(director => {
 
-    directorsList.innerHTML += '<li>' + director + '</li>';
+    directorsList.innerHTML += '<li><a href="#" onclick="getMoviesFromDirector(\'' + director + '\')">' + director + '</li></a>';
 
   });
+
+
+}
+
+function getMoviesFromDirector(director) {
+
+ const directorMovies = movies.filter(( movie => movie.director == director ));
+ 
+ mainContent.innerHTML = "";
+
+ mainContent.innerHTML = "<div id='content' style='grid-template-columns: auto;'><div id='hot'><h2>Peliculas dirigidas por " + director + " </h2><div><ul id='directorsList'></ul></div></div>"
+
+ directorMovies.forEach(movie => {
+
+   directorsList.innerHTML += '<li> ' + movie.title +'</li>';
+
+ });
 
 
 }
