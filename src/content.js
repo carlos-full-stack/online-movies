7d9db1089dc8116a9d9f0d690e1933d29a5dd4ae
Newbiecodeweaver <carlosgilino@gmail.com>
2023-04-19 11:45:11 +0200

    created content.js file to load page content dinamically

---
+++ src/content.js
@@ -0,0 +1,28 @@
+const content = 
+
+[
+    
+    {
+        name: "Home",
+        title: "Homepage",
+        html: `<div id="home-content">
+                <div id="hot">
+                    <h2>Novedades</h2>
+                    <div>Nuevas peliculas</div>
+                </div>
+                <div id="ramdom">
+                    <h2>Aleatorio</h2>
+                    <div>Peliculas aleatorias</div>
+                </div>
+                <div id="gender">
+                    <h2>Generos</h2>
+                    <div>Etiquetas generos</div>
+                </div>
+                <div id="popular">
+                    <h2>Populares</h2>
+                    <div>Peliculas populares</div>
+                </div>
+            </div>`
+    }
+
+];
